854502bc145691803efb6fdbbb613a5c4c6603e8
Prashun Chitrakar <pchitkr@gmail.com>
2022-03-08 20:56:35 +0545

    feat: add admin product list

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -8,7 +8,6 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
-  ORDER_PAY_RESET,
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_FAIL,
@@ -101,11 +100,7 @@ export const payOrder =
         },
       };
 
-      const { data } = await axios.put(
-        `/api/orders/${orderId}/pay`,
-        paymentResult,
-        config
-      );
+      await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config);
 
       dispatch({
         type: ORDER_PAY_SUCCESS,
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/productlist">
-                    <NavDropdown.Item>Proucts</NavDropdown.Item>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
                   <LinkContainer to="/admin/orderlist">
                     <NavDropdown.Item>Orders</NavDropdown.Item>
